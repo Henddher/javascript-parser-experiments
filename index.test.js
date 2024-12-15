4cc033ed2ab5ce5ecffb7c9cc714f3cd7aa170a7
Henddher Pedroza <henddher.pedroza@gmail.com>
2024-12-15 05:50:11 -0600

    use pegParse

--- index.test.js
+++ index.test.js
@@ -1,31 +1,32 @@
 const {pegParse, pegParse2} = require("./index.js");
+const parse = pegParse;
 
 test("dummy line", () => {
-    res = pegParse("dummy line");
+    res = parse("dummy line");
     expect(res).toEqual("dummy line");
 });
 test("a line\nanother line", () => {
-    res = pegParse("a line\nanother line");
+    res = parse("a line\nanother line");
     expect(res).toEqual("a line\nanother line");
 });
 test("a line\n\nanother line", () => {
-    res = pegParse("a line\n\nanother line");
+    res = parse("a line\n\nanother line");
     expect(res).toEqual("a line\n\nanother line");
 });
 test(" a line \n \n another line \n", () => {
-    res = pegParse(" a line \n \n another line \n");
+    res = parse(" a line \n \n another line \n");
     expect(res).toEqual("a line \n \n another line \n");
 });
 test("::row{} \n \n another line \n", () => {
-    res = pegParse(" ::row{} \n \n another line \n");
+    res = parse(" ::row{} \n \n another line \n");
     expect(res).toEqual(" \n \n another line \n");
 });
 test("::row{a='1'} \n \n another line \n", () => {
-    res = pegParse(" ::row{a='1'} \n \n another line \n");
+    res = parse(" ::row{a='1'} \n \n another line \n");
     expect(res).toEqual(" \n \n another line \n");
 });
 test(" ::quoted-text{author='Hamlet'  quote='To be or not to be ...'} \n \n another line \n", () => {
-    res = pegParse(" ::quoted-text{author='Hamlet'  quote='To be or not to be ...'} \n \n another line \n");
+    res = parse(" ::quoted-text{author='Hamlet'  quote='To be or not to be ...'} \n \n another line \n");
     expect(res).toEqual("To be or not to be ... - Hamlet \n \n another line \n");
 });
 // test("::quoted-text{quote='To be or not to be ... That is the question.'} \n \n another line \n", () => {
